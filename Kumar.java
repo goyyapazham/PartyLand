1103f330c001e52e82fb63675ba1e88142410eeb
goyyapazham <nsharadjaya@stuy.edu>
2016-01-14 14:46:25 -0500

    Word class structure

--- Kumar.java
+++ Kumar.java
@@ -1,37 +1,18 @@
 public class Kumar {
-    //instance variables of words
-    private String[] nouns = new String[]{"dog", "house", "cat"};
-    private String[] verbs = new String[]{"run", "greet", "walk", "jump", "play"};
-    private String[] adjective = new String[]{"slimy", "clean", "dirty", "big"};
-    private String[] adverbs = new String[]{"loudly", "kindly", "carelessly", "softly"};
+
+    protected String sentence;
  
-    public void Kumar(){
+    public Kumar() {
     }
 
     public String sentence() {
-	String nounPhrase;
-	String verbPhrase;
-	nounPhrase = nounPhrase();
-	verbPhrase = verbPhrase();
-	return nounPhrase + " "+ verbPhrase;
+	NounPhrase n = new NounPhrase();
+	VerbPhrase v = new VerbPhrase();
+	return n + " " + v;
     }
 
-    public String nounPhrase() {
-	String adj = "";
-	String noun;
-	for ( int x = 0; x < (int) (Math.random() * 5); x++ ) {
-	    adj += adjective[ (int) (Math.random() * adjective.length) ] + " ";
-	}
-	noun = nouns[ (int) (Math.random() * nouns.length) ];
-	return adj + noun;
-    }
-
-    public String verbPhrase() {
-	String adv;
-	String verb;
-	adv = adverbs[ (int) (Math.random() * adverbs.length) ];
-	verb = verbs[ (int) (Math.random() * verbs.length) ] + " ";
-	return verb + adv;
+    public String toString() {
+	return sentence;
     }
 
     public static void main( String[] args ) {
