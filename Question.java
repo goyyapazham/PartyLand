0604a91f2cf838742e4d7d2e3534e8f85ca4950f
goyyapazham <nsharadjaya@stuy.edu>
2016-01-16 16:49:05 -0500

    Question commenting

--- Question.java
+++ Question.java
@@ -1,9 +1,13 @@
 public class Question extends Sentence {
 
     public String generate() {
+	//noun-phrase can be! anything!
 	NounPhrase n = new NounPhrase();
+	//the noun-phrase may need an article... (if the subject doesn't allow for articles, the addArt() method won't add an article)
 	n.addArt();
+	//indicative mood: verbs are never "conjugated"
 	VerbPhrase v = new VerbPhrase(false);
+	//the verb "do" is ONLY conjugated for singular third-person subjects
 	if(n.subj < 3 || n.plural) {
 	    sentence = "do ";
 	}
