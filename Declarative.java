3623fe9b6e3bac67c448a6168264ff3fc22f481a
goyyapazham <nsharadjaya@stuy.edu>
2016-01-16 16:49:51 -0500

    Declarative commenting

--- Declarative.java
+++ Declarative.java
@@ -1,8 +1,11 @@
 public class Declarative extends Sentence {
 
     public String generate() {
+	//the noun-phrase can be anything!
 	NounPhrase n = new NounPhrase();
+	//if necessary, add article
 	n.addArt();
+	//verbs are only conjugated for singular third-person subjects
 	VerbPhrase v = new VerbPhrase(n.subj == 3 && !(n.plural));
 	sentence = n + " " + v;
 	punctuate();
