c32ebb56f6e524096b54ead7553aecb04339c39c
goyyapazham <nsharadjaya@stuy.edu>
2016-01-16 16:44:27 -0500

    NounPhrase commenting

--- NounPhrase.java
+++ NounPhrase.java
@@ -8,32 +8,41 @@ public class NounPhrase extends Phrase {
     protected int subj = 3;
     protected boolean plural = false;
 
+    //constructor
     public NounPhrase() {
 	this((int)(Math.random() * 3) == 0);
     }
-    
+
+    //if nouns /must/ be pluralized
     public NounPhrase(boolean boo) {
-	//select random noun from array of nouns
 	int getSubj = (int)(Math.random() * 8);
+	//FIRST: identify /subject/ word and its point-of-view integer
+	//12.5% chance of getting first-person
 	if(getSubj == 0) {
 	    if(boo) word = "we";
 	    else word = "I";
 	    subj = 1;
 	}
+	//12.5% chance of getting second-person
 	else if (getSubj == 1) {
 	    word = "you";
 	    subj = 2;
 	}
+	//75% chance of getting third-person
 	else {
 	    word = nouns[(int)(Math.random() * nouns.length)];
 	    if(boo) pluralize();
 	}
+	//SECOND: transfer word to phrase
 	phrase = word;
+	//THIRD: commence adjective addition, if necessary
         addAdj();
     }
 
+    //if Sentence type is subject-specific
     public NounPhrase(int i, boolean boo) {
 	subj = i;
+	//FIRST: identify /subject/ word and its point-of-view integer
 	if(subj == 1) {
 	    if(boo) word = "we";
 	    else word = "I";
@@ -45,7 +54,9 @@ public class NounPhrase extends Phrase {
 	    word = nouns[(int)(Math.random() * nouns.length)];
 	    if(boo) pluralize();
 	}
+	//SECOND: transfer word to phrase
 	phrase = word;
+	//THIRD: commence adjective addition, if necessary
 	addAdj();
     }
 
@@ -62,7 +73,9 @@ public class NounPhrase extends Phrase {
 	}
     }
 
+    //never used in NounPhrase bc only certain sentence types require articles
     public void addArt() {
+	//only third-person nouns require articles
 	if(subj == 3) phrase = "the " + phrase;
     }
 
@@ -72,30 +85,29 @@ public class NounPhrase extends Phrase {
 	if (word.equals("I")) {
 	    word = "we";
 	}
+	//pluralization via "-es"
 	else if (word.substring(word.length() - 2).equals("ch")
 	    || word.substring(word.length() - 2).equals("sh")
 	    || word.substring(word.length() - 1).equals("x")
 	    || word.substring(word.length() - 1).equals("s")
 	    || word.substring(word.length() - 1).equals("z")) {
-	    //it's pluralized via "-es"
 	    word += "es";
 	}
-	//if word ends in y
+	//pluralization via "-y" --> "-ies"
 	else if (word.substring(word.length() - 1).equals("y")) {
-	    //it's pluralized via dropping "y" adding "-ies"
 	    word = word.substring(0,word.length() - 1) + "ies";
 	}
+	//pluralization via "-s"
 	else {
-	    //otherwise, it's pluralized via "-s"
 	    word += "s";
 	}
-	//definitely need more cases, but solid enough for now
     }
     
     public String toString() {
 	return phrase;
     }
-    
+
+    //~~~ test cases for debugging purposes ~~~
     /*
     public static void main(String[] args) {
 	NounPhrase pardeep = new NounPhrase();
