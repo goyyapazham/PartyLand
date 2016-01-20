410e42bc7427dc3741d2c1e76d940e6bf95f39f8
goyyapazham <nsharadjaya@stuy.edu>
2016-01-19 20:48:01 -0500

    for manageability of code, CSV wrapper function

--- Phrase.java
+++ Phrase.java
@@ -3,4 +3,9 @@ public abstract class Phrase {
     protected String phrase;
     protected String word;
 
+    public String[] parseCSV(String filename) {
+	ParseCSV f = new ParseCSV(filename);
+	return f.words;
+    }
+
 }
