7724156c3f1db378e141a229cbb8b5d14d021f7e
singhpardeep01 <psingh@stuy.edu>
2016-01-24 09:34:17 -0500

    restructuring answering order

--- Kumar.java
+++ Kumar.java
@@ -194,6 +194,56 @@ public class Kumar {
 	    singular.startConnection();
 	    singular.singular();
 	    str[i] = singular.toString();
+	    if (str[i].equals("Do you even English?")) {
+		s.sentence = "Do you even English?";
+		return s;
+	    }
+	    if(objects.contains(str[i])) {
+		d.generate(str[i]);
+		b = true;
+		break;
+	    }
+	}
+	if (b == false) return randAnswer(s);
+	else return d;
+    }
+    public boolean isSpecific( Sentence s ) {
+	String[] str = s.sentence.split(" ");
+	str[str.length - 1] = strip(str[str.length - 1]);
+	//singular version of all objects in input
+	for(int i = 0; i < str.length; i++) {
+	    if ( isPunc( str[i].substring( str[i].length() - 1 ) ) ) {
+		str[i] = str[i].substring( 0, str[i].length() - 1 );
+	    }
+	    HTMLParser singular = new HTMLParser( str[i] );
+	    singular.startConnection();
+	    singular.singular();
+	    str[i] = singular.toString();
+	    if ( str[i].equals("color") ||
+		 str[i].equals("animal") ||
+		 str[i].equals("food") ||
+		 colors.contains(str[i]) ||
+		 foods.contains(str[i]) ||
+		 animals.contains(str[i]) ) {
+		return true;
+	    }
+	}
+	return false;
+    }
+    public Sentence specific( Sentence s ) {
+	Declarative d = new Declarative();
+	Boolean b = false;
+	String[] str = s.sentence.split(" ");
+	str[str.length - 1] = strip(str[str.length - 1]);
+	//singular version of all objects in input
+	for(int i = 0; i < str.length; i++) {
+	    if ( isPunc( str[i].substring( str[i].length() - 1 ) ) ) {
+		str[i] = str[i].substring( 0, str[i].length() - 1 );
+	    }
+	    HTMLParser singular = new HTMLParser( str[i] );
+	    singular.startConnection();
+	    singular.singular();
+	    str[i] = singular.toString();
 	    if ( str[i].equals("color") ||
 		 str[i].equals("animal") ||
 		 str[i].equals("food") ) {
@@ -224,20 +274,9 @@ public class Kumar {
 		s.sentence += " " + animal2 + ".";
 		return s;
 	    }
-	    if (str[i].equals("Do you even English?")) {
-		s.sentence = "Do you even English?";
-		return s;
-	    }
-	    if(objects.contains(str[i])) {
-		d.generate(str[i]);
-		b = true;
-		break;
-	    }
 	}
-	if (b == false) return randAnswer(s);
-	else return d;
+	return s;
     }
-    
     //if all else fails
     public Sentence randAnswer(Sentence s) {
 	int rand = (int)(Math.random() * 2);
@@ -258,6 +297,7 @@ public class Kumar {
 	    s = Keyboard.readString();
 	    input = new Input(s);
 	    if ( isGreet(input) ) response = greet(input);
+	    else if( isSpecific(input) ) response = specific(input);
 	    else if(isQuestion(input.sentence)) response = basicQAnswer(input);
 	    else if ( isFarewell(input) ) {
 		response = farewell(input);
