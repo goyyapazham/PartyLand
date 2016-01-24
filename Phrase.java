f4a852c7a8f2d2d8a1809dd81370d4fcb9cb7997
singhpardeep01 <psingh@stuy.edu>
2016-01-24 16:44:48 -0500

    commenting Phrase

--- Phrase.java
+++ Phrase.java
@@ -3,6 +3,8 @@ public abstract class Phrase {
     protected String phrase;
     protected String word;
 
+    //method to access files with nouns and verbs to be
+    //inherited by NounPhrase and VerbPhrase
     public String[] parseCSV(String filename) {
 	ParseCSV f = new ParseCSV(filename);
 	String[] retArr = new String[f.words.size()];
