2030fe6457d4de86dc1fc539b11a222e70cafc50
singhpardeep01 <psingh@stuy.edu>
2016-01-24 17:36:42 -0500

    commenting Where

--- Where.java
+++ Where.java
@@ -2,7 +2,7 @@ import java.lang.ArrayIndexOutOfBoundsException;
 import java.util.ArrayList;
 
 public class Where extends Sentence {
-
+    //instance variables refering to locations form locations.txt
     ParseCSV l = new ParseCSV("locations.txt");
     ArrayList locationstxt = l.words;
     ArrayList<String> cities = new ArrayList<String>();
@@ -15,7 +15,7 @@ public class Where extends Sentence {
 	sentence = generate( input.toString() );
 	punctuate();
     }
-
+    //strips string of punctuation
     public static String strip(String s) {
 	String retStr = "";
 	for(int i = 0; i < s.length(); i++) {
@@ -25,7 +25,7 @@ public class Where extends Sentence {
 	}
 	return retStr;
     }
-
+    //organizes locations by state, city country
     public void modifyLocations() {
 	for(int i = 0; i < locationstxt.size(); i++) {
 	    locationstxt.set(i, ((String)locationstxt.get(i)).split(", "));
@@ -43,16 +43,22 @@ public class Where extends Sentence {
     }
 
     public String generate(String s) {
+	//strip input of punctuation
 	String z = strip(s);
+	//split by spaces
 	String[] y = z.split(" ");
+	//convert to ArrayList
 	ArrayList<String> input = new ArrayList<String>();
 	for(int i = 0; i < y.length; i++) {
 	    input.add(y[i]);
 	}
+	//organize locations
 	modifyLocations();
 	String retStr;
 	for(int i = 0; i < input.size(); i++) {
+	    //make locations properly capitilized
 	    String x = properNounIfy(input.get(i));
+	    //if input it a city ask if its in a country + state or just country
 	    if(cities.contains(x)) {
 	        retStr = "Isn't that in ";
 		String country = countries.get(cities.indexOf(x));
@@ -63,6 +69,7 @@ public class Where extends Sentence {
 		    return retStr + country;
 	    }
 	}
+	//if not tell them to use google Maps or try and another city
 	retStr = "I'm not sure. Have you tried ";
 	if((int)(Math.random() * 2) == 0) {
 	    retStr += cities.get((int)(Math.random() * cities.size()));
@@ -72,7 +79,7 @@ public class Where extends Sentence {
 	}
 	return retStr;
     }
-
+    //capitilize first letter in string
     public String properNounIfy(String s) {
 	return s.substring(0, 1).toUpperCase() + s.substring(1);
     }
