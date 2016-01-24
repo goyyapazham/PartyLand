8fc0a15dabd71fb9ca50561b792dc9f16124ae4c
goyyapazham <nsharadjaya@stuy.edu>
2016-01-23 21:16:04 -0500

    Where.java updated

--- Where.java
+++ Where.java
@@ -1,3 +1,4 @@
+import java.lang.ArrayIndexOutOfBoundsException;
 import java.util.ArrayList;
 
 public class Where extends Sentence {
@@ -27,6 +28,53 @@ public class Where extends Sentence {
 	    }
 	}
 	return retStr;
+    }
+
+    public void modifyLocations() {
+	for(int i = 0; i < locationstxt.size(); i++) {
+	    locationstxt.set(i, ((String)locationstxt.get(i)).split(", "));
+	}
+	for(int i = 0; i < locationstxt.size(); i++) {
+	    cities.add(((String[])locationstxt.get(i))[0]);
+	    countries.add(((String[])locationstxt.get(i))[1]);
+	    try {
+		states.add(((String[])locationstxt.get(i))[2]);
+	    }
+	    catch(ArrayIndexOutOfBoundsException e) {
+		states.add("");
+	    }
+	}
+    }
+
+    public String generate(String s) {
+	String z = strip(s);
+	String[] y = z.split(" ");
+	ArrayList<String> input = new ArrayList<String>();
+	for(int i = 0; i < y.length; i++) {
+	    y[i] = y[i].toLowerCase();
+	    input.add(y[i]);
+	}
+	modifyLocations();
+	String retStr;
+	for(int i = 0; i < input.size(); i++) {
+	    String x = input.get(i);
+	    if(cities.contains(x)) {
+	        retStr = "Isn't that in ";
+		country = countries.get(cities.indexOf(x));
+		state = states.get(cities.indexOf(x));
+		if(!(state).equals(""))
+		    return retStr + state;
+		else
+		    return retStr + country;
+	    }
+	}
+	retStr = "I'm not sure. Have you tried ";
+	if((int)(Math.random() * 2) == 0) {
+	    retStr += cities.get((int)(Math.random() * cities.size()));
+	}
+	else {
+	    retStr += "Google Maps";
+	}
     }
     
     public void punctuate() {
