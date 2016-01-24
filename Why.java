127b46c77aeed9657a8070939ae8e5bb9d9ec01a
singhpardeep01 <psingh@stuy.edu>
2016-01-24 01:13:10 -0500

    why class responds now

--- Why.java
+++ Why.java
@@ -1,7 +1,10 @@
+import java.util.ArrayList;
+
 public class Why extends Sentence {
 
     private static String punctuation = ".,;:!?";
-
+    ParseCSV f = new ParseCSV("objects.txt");
+    private ArrayList<String> objects = f.words;
     public Why( Sentence input ) {
 	sentence = generate( input.toString() );
 	punctuate();
@@ -16,9 +19,43 @@ public class Why extends Sentence {
 	}
 	return retStr;
     }
-    
+    public static boolean isPunc( String searching ) {
+     	for ( int i = 0; i < punctuation.length(); i++) {
+	    if ( searching.equals( punctuation.substring( i, i+1 ) ) ) {
+		return true;
+	    }
+	}
+	return false;
+    }
+
     public String generate(String s) {
-	return "That's the question";
+	Declarative retDec = new Declarative();
+	Boolean b = false;
+	String[] str = s.split(" ");
+	str[str.length - 1] = strip(str[str.length - 1]);
+	for(int i = 0; i < str.length; i++) {
+	    if ( isPunc( str[i].substring( str[i].length() - 1 ) ) ) {
+		str[i] = str[i].substring( 0, str[i].length() - 1 );
+	    }
+	    HTMLParser singular = new HTMLParser( str[i] );
+	    singular.startConnection();
+	    singular.singular();
+	    str[i] = singular.toString();
+	    if ( str[i].equals("color") ||
+		 str[i].equals("animal") ||
+		 str[i].equals("food") ) {
+		What answer = new What(s);
+		return answer.toString();
+	    }
+	    if(objects.contains(str[i])) {
+		retDec.generate(str[i]);
+		b = true;
+		break;
+	    }
+	}
+	retDec.sentence = strip( retDec.toString().toLowerCase() );
+	if (b) return "Because " + retDec;
+	else return "Because I said so";
     }
     
     public void punctuate() {
