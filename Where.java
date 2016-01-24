54836fb6944bb5196e321158b10b3a54f08257fc
goyyapazham <nsharadjaya@stuy.edu>
2016-01-23 21:01:48 -0500

    Where.java started

--- Where.java
+++ Where.java
@@ -1,4 +1,14 @@
+import java.util.ArrayList;
+
 public class Where extends Sentence {
+
+    ParseCSV l = new ParseCSV("locations.txt");
+    ArrayList locationstxt = l.words;
+    ArrayList<String> cities = new ArrayList<String>();
+    ArrayList<String> countries = new ArrayList<String>();
+    ArrayList<String> states = new ArrayList<String>();
+
+    private static String punctuation = ".,;:!?";
 
     public Where( Sentence input ) {
 	sentence = generate( input.toString() );
@@ -7,6 +17,16 @@ public class Where extends Sentence {
 
     public String generate(String s) {
 	return "Have you tried Google Maps";
+    }
+
+    public static String strip(String s) {
+	String retStr = "";
+	for(int i = 0; i < s.length(); i++) {
+	    if (punctuation.indexOf(s.substring(i, i + 1)) == -1) {
+		retStr += s.substring(i, i + 1);
+	    }
+	}
+	return retStr;
     }
     
     public void punctuate() {
