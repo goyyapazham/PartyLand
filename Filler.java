078fc68af0156adf1850ac895f82e522e0c71923
goyyapazham <nsharadjaya@stuy.edu>
2016-01-24 09:30:34 -0500

    debugging

--- Filler.java
+++ Filler.java
@@ -1,18 +1,14 @@
 public class Filler extends Sentence {
 
     private String[] responses = parseCSV("responses.txt");
-
-    public String[] parseCSV(String filename) {
-	ParseCSV f = new ParseCSV(filename);
-	String[] retArr = new String[f.words.size()];
-	for(int i = 0; i < f.words.size(); i++) {
-	    retArr[i] = f.words.get(i);
-	}
-	return retArr;
-    }
+    private String[] fillers = parseCSV("fillers.txt");
     
     public String generate(String s) {
-	return responses[(int)(Math.random() * responses.length)];
+	if(Kumar.isQuestion(s))
+	    s = responses[(int)(Math.random() * responses.length)];
+	else
+	    s = fillers[(int)(Math.random() * fillers.length)];
+	return s;
     }
 
     public void punctuate() {
