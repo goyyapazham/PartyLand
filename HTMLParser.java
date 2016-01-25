01508a68dc101913b49ae58ad19cf07f6f2c7bb9
singhpardeep01 <psingh@stuy.edu>
2016-01-24 19:01:46 -0500

    HTMLParser clean up and commenting

--- HTMLParser.java
+++ HTMLParser.java
@@ -5,16 +5,18 @@ import java.lang.Throwable;
 
 public class HTMLParser {
 
+    //HTML from site
     protected String HTML;
+    //word being searched
     protected String word;
+    //url of side
     protected String URL;
+    //true when word is not found
     protected boolean unknown;
     //pluralize
     protected String keyHTML = "<span class=\"dbox-pg\">noun</span>, <span class=\"dbox-pg\">plural </span>";
     //singular
     protected String keyHTML2 = "<h1 class=\"head-entry\"><span class=\"me\" data-syllable=\"";
-    //suggestions
-    protected String keyHTML3 = "<span class=\"me\" data-syllable=\"";
 
     public HTMLParser( String w ) {
 	HTML = "";
@@ -24,42 +26,33 @@ public class HTMLParser {
     }
 
     public String toString() {
+	//returns the pluralized or singularized word
 	return word;
     }
-    
+
+    //opens connection to website and pulls data
     public void startConnection() {
 	URLConnection connection = null;
 	try {
+	    //opens the connection to the URL
 	    connection = new URL(URL).openConnection();
-	    //System.out.println(connection);
+	    //Scanner with the InputStream from the website
 	    Scanner scanner = new Scanner(connection.getInputStream());
-	    //System.out.println("error2");
 	    scanner.useDelimiter("\\Z");
-	    //System.out.println("error3");
+	    //go through the input stream and set the HTML to it
 	    HTML = scanner.next();
-	    //System.out.println("error4");
 	}
+	//if connection failed because word does not exist
 	catch ( Exception e ) {
-	    /*
-	    System.out.println("Did you mean?");
-	    URL = "http://dictionary.reference.com/misspelling?term=" + word.toLowerCase() + "&s=t";
-	    System.out.println(URL);
-	    startConnection();
-	    System.out.println("Success!");
-	    suggestion();
-	    System.out.println(word);
-	    URL =  "http://dictionary.reference.com/browse/" + word.toLowerCase() + "?s=t";
-	    System.out.println(URL);
-	    startConnection();
-	    */
 	    unknown = true;
 	}
     }
-
+    //returns if word is unknown
     public boolean getUnknown() {
 	return unknown;
     }
-    
+
+    //searches for the string searching in the input and returns the index where the string begins
     public int search( String searching, String input ) {
 	int length = searching.length();
 	input = input.toLowerCase();
@@ -70,33 +63,32 @@ public class HTMLParser {
 	}
 	return -1;
     }
-    
+    //used to find plural word
     public void plural() {
+	//if the keyHTML is found
 	if ( search( keyHTML, HTML ) > 0 ) {
+	    //x is the index at where the key HTML starts
 	    int x = search( keyHTML, HTML );
+	    //pluralLocation is where the key HTML tag ends and the plural word begins
 	    int pluralLocation = search( ">", HTML.substring(x) ) + x;
+	    //pluralLocationEnd is where the plural word ends and the next tag begins
 	    int pluralLocationEnd = search( "<", HTML.substring(pluralLocation) ) + pluralLocation - 2;
+	    //the word is set to the HTML substring where the plural word is located
 	    word = HTML.substring( pluralLocation, pluralLocationEnd );
 	}
     }
     public void singular() {
+	//if the keyHTML is found
 	if ( search( keyHTML2, HTML ) > 0 ) {
+	    //x is the index at where the key HTML starts
 	    int x = search( keyHTML2, HTML );
+	    //singularLocation is where the key HTML tag ends and the singular word begins
 	    int singularLocation = search( ">", HTML.substring(x) ) + x;
+	    //singularLocationEnd is where the singular word ends and the next tag begins
 	    int singularLocationEnd = search( "<", HTML.substring(singularLocation) ) + singularLocation - 1;
+	    //the word is set to the HTML substring where the singular word is located
 	    word = HTML.substring( singularLocation, singularLocationEnd );
 	}
-    }
-
-    public void suggestion() {
-	if ( search( keyHTML3, HTML ) > 0 ) {
-	    int x = search( keyHTML3, HTML );
-	    int suggestionLocation = search( ">", HTML.substring(x) ) + x;
-	    int suggestionLocationEnd = search( "<", HTML.substring(suggestionLocation) ) + suggestionLocation - 1;
-	    word = HTML.substring( suggestionLocation, suggestionLocationEnd );
-	    System.out.println(word);
-	}
-	System.out.println(word);
     }
     
     public static void main( String[] args ) {
